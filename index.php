62a9e1c3c39bc643cea3caaee328c536d80aa6ac
Darío López <daloga02@floridauniversitaria.es>
2025-02-05 22:37:39 +0100

    32 cambio

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
  <div class="container">
  <h1>Generador aleatorio de Hola Mundos Dario 22</h1>
  <form method="post">
- <button type="submit">Generadsaddsar1</button>
+ <button type="submit">Generadssaddsar1</button>
  </form>
  <?php if (!empty($mensaje))
  echo "<p><b>".$codigo.":</b> ".htmlspecialchars($mensaje)."</p>";
